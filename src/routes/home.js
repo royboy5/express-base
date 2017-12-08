d211996334dcdd95ed6a17b1582edfb069fdd647
roy <roye19@gmail.com>
2017-12-08 14:52:49 -0500

    removed auth from home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,13 +4,8 @@ const log = require('../utils/logger')
 
 const express = require('express')
 const router = new express.Router()
-const authController = require('../controllers/authController')
-const passportService = require('../utils/passport')
-const passport = require('passport')
 
-const requireAuth = passport.authenticate('jwt', { session: false })
-
-router.get('/', requireAuth, (req, res) => {
+router.get('/', (req, res) => {
   log.info('Accessed /')
   res.send('Home')
 })
